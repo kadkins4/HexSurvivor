876c7671c00dd38dea3be9b00b97a2fc896b71d0
Kendall Adkins <adkins.kendall90@gmail.com>
2025-12-24 14:18:36 -0500

    update constants - balance initial

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 // PLAYER
 export const PLAYER_BASE_MAX_HP = 100;
 export const PLAYER_BASE_RADIUS = 24;
-export const PLAYER_BASE_DAMAGE = 10;
-export const PLAYER_BASE_FIRE_RATE = 1.5;
-export const PLAYER_BASE_RANGE = 250;
+export const PLAYER_BASE_DAMAGE = 8;
+export const PLAYER_BASE_FIRE_RATE = 1;
+export const PLAYER_BASE_RANGE = 200;
 
 // PROJECTILE
 export const PROJECTILE_SPEED = 400;
@@ -27,26 +27,26 @@ export const PLAYER_HITFLASH_DURATION = 0.18;
 export const FLOATING_TEXT_LIFETIME = 0.9;
 
 // Wave / spawning
-export const BASE_ENEMIES_SPAWNED = 4;
+export const BASE_ENEMIES_SPAWNED = 0;
 export const WAVE_SPAWN_INTERVAL = 0.3;
 
 // ENEMY (defaults)
 export const ENEMY_BASE_HP = 12;
-export const ENEMY_BASE_SPEED = 40;
+export const ENEMY_BASE_SPEED = 35;
 export const ENEMY_BASE_RADIUS = 12;
 export const ENEMY_CONTACT_DPS = 10;
 
 // DRONE (standard enemy)
 export const DRONE_HP = 15;
-export const DRONE_SPEED = 50;
+export const DRONE_SPEED = 45;
 export const DRONE_RADIUS = 10;
 
 // STRIKER (charger)
 export const STRIKER_HP = 10;
 export const STRIKER_APPROACH_SPEED = 35; // slow approach
-export const STRIKER_DASH_SPEED = 320; // fast ram
-export const STRIKER_CHARGE_RANGE = 160; // start charging when within this
-export const STRIKER_CHARGE_TIME = 0.6; // seconds to charge
+export const STRIKER_DASH_SPEED = 300; // fast ram
+export const STRIKER_CHARGE_RANGE = 175; // start charging when within this
+export const STRIKER_CHARGE_TIME = 1; // seconds to charge
 export const STRIKER_RADIUS = 8;
 export const STRIKER_EXPLODE_DAMAGE = 24;
 export const STRIKER_STATUS = {
